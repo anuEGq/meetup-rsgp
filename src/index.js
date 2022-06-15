d6a603744dcc0029c9ecf21fb3b78d9fbcaf402c
anuEGq <103645667+anuEGq@users.noreply.github.com>
2022-06-15 16:25:30 +0530

    Update index.js
    comment added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <FavoriteContextProvider>
     <BrowserRouter>
-    <App />
+    <App />/*hi*/
     </BrowserRouter>
     </FavoriteContextProvider>
   </React.StrictMode>
